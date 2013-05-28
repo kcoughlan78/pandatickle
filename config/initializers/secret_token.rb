d3ace801117a383fcc1031eeb5417fa36ff0a171
Kieran Coughlan <kcoughlan78@me.com>
2013-05-28 18:50:38 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pandatickle::Application.config.secret_token = 'e4922181e19448be6d08c9534deadd42cc325aa09017727f3c61402a7f664526208fe096ef27559512f1c4a1ed91488ff550eca3bbc40fa84662352f8268703c'
