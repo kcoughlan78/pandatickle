2f07001b7c41a289a405a1b1404871e14487412c
Kieran Coughlan <kcoughlan78@me.com>
2013-06-02 18:13:11 +0100

    changed session create and user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url, notice: "Signed in."
   end
-
-
 
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Signed out."
   end
-
-
 end
