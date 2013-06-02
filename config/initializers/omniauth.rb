735356d27551c8739bd68344cfc2b6b657c68269
Kieran Coughlan <kcoughlan78@me.com>
2013-06-02 13:39:07 +0100

    added token and key for oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :developer unless Rails.env.production?
+  provider :twitter, ENV['sMpfbcJVWmSSCL85C2VwZQ'], ENV['SN2bu6HOsC2UIshdTnjnn6M1JhA0jYkMR5p7v7SBhE']
 end
