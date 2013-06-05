3b10e4cb94d7295c613e5e4e5a9364a5b15ef350
Kieran Coughlan <kcoughlan78@me.com>
2013-06-06 00:13:39 +0100

    added css files to assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Pandatickle::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( Sprite3D.js intro.js pandatickle.js.erb  )
+  config.assets.precompile += %w( Sprite3D.js intro.js pandatickle.js.erb facts.css aboutapp.css home.css games.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
